bc6bd6dc294503c2e770f440d569e8e48c45e41d
Renaux <renaux@et.esiea.fr>
2018-05-25 19:09:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/charles/recettons/Food.java
@@ -0,0 +1,14 @@
+package com.example.charles.recettons;
+
+public class Food {
+
+    private static String foodName;
+
+    public static String getFoodName() {
+        return foodName;
+    }
+
+    public static void setFoodName(String name) {
+        foodName = name;
+    }
+}
